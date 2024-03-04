0e609be8b5896559e43a39517e3f4bff47a2b0fc
TienNH21 <tiennh21@fpt.edu.vn>
2024-03-04 13:52:19 +0700

    Lesson 1

---
+++ src/sd19313_mob1024_sp24b2/SD19313_MOB1024_SP24B2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sd19313_mob1024_sp24b2;
+
+/**
+ *
+ * @author tiennh
+ */
+public class SD19313_MOB1024_SP24B2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
