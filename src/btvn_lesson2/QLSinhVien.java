f18eec4e8f2ef018e61c8b5acb4d4f6dd3301c43
TienNH21 <tiennh21@fpt.edu.vn>
2024-03-11 13:38:55 +0700

    QLSV CRUD Full

--- src/btvn_lesson2/QLSinhVien.java
+++ src/btvn_lesson2/QLSinhVien.java
@@ -1,9 +1,38 @@
 package btvn_lesson2;
 
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
 public class QLSinhVien extends javax.swing.JFrame {
+    private ArrayList<SinhVien> listSV = new ArrayList<>();
 
     public QLSinhVien() {
         initComponents();
+        
+        this.listSV.add(new SinhVien("PH1", "A", "Nam", "Java"));
+        this.listSV.add(new SinhVien("PH2", "B", "Nu", "UDPM"));
+        this.listSV.add(new SinhVien("PH3", "C", "Nam", "XLDL"));
+        this.listSV.add(new SinhVien("PH4", "D", "Nam", "C#"));
+
+        this.loadTable();
+    }
+    
+    private void loadTable()
+    {
+        DefaultTableModel dtm =
+            (DefaultTableModel) this.tblSV.getModel();
+        
+        dtm.setRowCount(0);
+        for (SinhVien sv : listSV) {
+            Object[] row = {
+                sv.getMa(),
+                sv.getTen(),
+                sv.getGioiTinh(),
+                sv.getChuyenNganh(),
+            };
+            
+            dtm.addRow(row);
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -17,12 +46,12 @@ public class QLSinhVien extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         txtMaSV = new javax.swing.JTextField();
         txtHoTen = new javax.swing.JTextField();
-        txtCNganh = new javax.swing.JTextField();
         txtGioiTinh = new javax.swing.JTextField();
         btnThem = new javax.swing.JButton();
         btnSua = new javax.swing.JButton();
         btnXoa = new javax.swing.JButton();
         btnLamMoi = new javax.swing.JButton();
+        cbbCNganh = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblSV = new javax.swing.JTable();
 
@@ -39,12 +68,34 @@ public class QLSinhVien extends javax.swing.JFrame {
         jLabel4.setText("C/Ngành");
 
         btnThem.setText("Thêm");
+        btnThem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnThemActionPerformed(evt);
+            }
+        });
 
         btnSua.setText("Sửa");
+        btnSua.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSuaActionPerformed(evt);
+            }
+        });
 
         btnXoa.setText("Xóa");
+        btnXoa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnXoaActionPerformed(evt);
+            }
+        });
 
         btnLamMoi.setText("Làm mới");
+        btnLamMoi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLamMoiActionPerformed(evt);
+            }
+        });
+
+        cbbCNganh.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Java", "C#", "XLDL", "UDPM" }));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -71,8 +122,8 @@ public class QLSinhVien extends javax.swing.JFrame {
                                 .addComponent(txtGioiTinh, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel4)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(txtCNganh))))
+                                .addGap(18, 18, 18)
+                                .addComponent(cbbCNganh, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(btnThem)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -97,7 +148,7 @@ public class QLSinhVien extends javax.swing.JFrame {
                     .addComponent(jLabel2)
                     .addComponent(jLabel4)
                     .addComponent(txtHoTen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtCNganh, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cbbCNganh, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnThem)
@@ -115,6 +166,11 @@ public class QLSinhVien extends javax.swing.JFrame {
                 "Mã SV", "Họ Tên", "Giới tính", "C/Ngành"
             }
         ));
+        tblSV.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblSVMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tblSV);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -141,6 +197,83 @@ public class QLSinhVien extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void tblSVMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblSVMouseClicked
+        int row = this.tblSV.getSelectedRow();
+        
+        if (row == -1) {
+            return ;
+        }
+        
+        String ma = this.tblSV.getValueAt(row, 0).toString();
+        String ten = this.tblSV.getValueAt(row, 1).toString();
+        String gioiTinh = this.tblSV.getValueAt(row, 2).toString();
+        String cNganh = this.tblSV.getValueAt(row, 3).toString();
+        
+        this.txtMaSV.setText(ma);
+        this.txtHoTen.setText(ten);
+        this.txtGioiTinh.setText(gioiTinh);
+        this.cbbCNganh.setSelectedItem(cNganh);
+    }//GEN-LAST:event_tblSVMouseClicked
+
+    private void btnLamMoiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLamMoiActionPerformed
+        this.clearForm();
+    }//GEN-LAST:event_btnLamMoiActionPerformed
+
+    private void btnXoaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnXoaActionPerformed
+        int row = this.tblSV.getSelectedRow();
+        
+        if (row == -1) {
+            return ;
+        }
+        
+        String ma = this.tblSV.getValueAt(row, 0).toString();
+        
+        for (int i = 0; i < this.listSV.size(); i++) {
+            SinhVien sv = this.listSV.get(i);
+            if (sv.getMa().equals(ma)) {
+                this.listSV.remove(i);
+                break;
+            }
+        }
+        
+        this.loadTable();
+        this.clearForm();
+    }//GEN-LAST:event_btnXoaActionPerformed
+
+    private void btnThemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThemActionPerformed
+        String ma = this.txtMaSV.getText();
+        String ten = this.txtMaSV.getText();
+        String gt = this.txtMaSV.getText();
+        String cNganh = this.cbbCNganh.getSelectedItem().toString();
+        SinhVien sv = new SinhVien(ma, ten, gt, cNganh);
+        this.listSV.add(sv);
+        this.loadTable();
+        this.clearForm();
+    }//GEN-LAST:event_btnThemActionPerformed
+
+    private void btnSuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSuaActionPerformed
+        String ma = this.txtMaSV.getText();
+        String ten = this.txtMaSV.getText();
+        String gt = this.txtMaSV.getText();
+        String cNganh = this.cbbCNganh.getSelectedItem().toString();
+        SinhVien newSV = new SinhVien(ma, ten, gt, cNganh);
+        
+        for (int i = 0; i < this.listSV.size(); i++) {
+            SinhVien oldSV = this.listSV.get(i);
+            if (oldSV.getMa().equals(newSV.getMa())) {
+                this.listSV.set(i, newSV);
+            }
+        }
+    }//GEN-LAST:event_btnSuaActionPerformed
+
+    private void clearForm()
+    {
+        this.txtMaSV.setText("");
+        this.txtHoTen.setText("");
+        this.txtGioiTinh.setText("");
+        this.cbbCNganh.setSelectedIndex(0);
+    }
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -178,6 +311,7 @@ public class QLSinhVien extends javax.swing.JFrame {
     private javax.swing.JButton btnSua;
     private javax.swing.JButton btnThem;
     private javax.swing.JButton btnXoa;
+    private javax.swing.JComboBox<String> cbbCNganh;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -185,7 +319,6 @@ public class QLSinhVien extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblSV;
-    private javax.swing.JTextField txtCNganh;
     private javax.swing.JTextField txtGioiTinh;
     private javax.swing.JTextField txtHoTen;
     private javax.swing.JTextField txtMaSV;
---
+++ src/btvn_lesson2/SinhVien.java
@@ -0,0 +1,52 @@
+package btvn_lesson2;
+
+public class SinhVien {
+    private String ma;
+    private String ten;
+    private String gioiTinh;
+    private String chuyenNganh;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(String ma, String ten, String gioiTinh, String chuyenNganh) {
+        this.ma = ma;
+        this.ten = ten;
+        this.gioiTinh = gioiTinh;
+        this.chuyenNganh = chuyenNganh;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getChuyenNganh() {
+        return chuyenNganh;
+    }
+
+    public void setChuyenNganh(String chuyenNganh) {
+        this.chuyenNganh = chuyenNganh;
+    }
+    
+    
+}
